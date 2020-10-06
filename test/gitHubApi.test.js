8db48156f9949664227f255f5b9cb6021d92210a
[Tahir Qayyum] <[qayyumtahir00@gmail.com]>
2020-10-06 10:39:34 -0400

    added chai-http and chai to test the status code

--- test/gitHubApi.test.js
+++ test/gitHubApi.test.js
@@ -1,7 +1,12 @@
 const Api = require('../src/gitHub-Api')
 const { expect } = require('chai');
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should
 
-describe("Dark Sky API", async () => {
+chai.use(chaiHttp)
+
+describe("GitHub User api", async () => {
 
   let api;
 
@@ -9,10 +14,53 @@ describe("Dark Sky API", async () => {
     api = new Api();
   })
 
-  it.only("Can get forecast info", async () => {
+  it.skip("Api property login - tqayyum", async () => {
     const response = await api.getApi();
+    const expVal = 'tqayyum'
 
-    console.log(response);
+    expect(response.login).to.equal(expVal)
+  })
+  
+  it.skip("Api property company - null", async () => {
+    const response = await api.getApi();
+    const expVal = null
+
+    expect(response.company).to.equal(expVal)
   })
 
+  it.skip("Api property site_admin - false", async () => {
+    const response = await api.getApi();
+    const expVal = false
+
+    expect(response.site_admin).to.equal(expVal)
+  })
+
+  it.skip("Api property blog - empty", async () => {
+    const response = await api.getApi();
+
+    expect(response.blog).to.empty
+  })
+
+  // Negative Test
+  it.skip("Api property site_admin - true", async () => {
+    const response = await api.getApi();
+    const expVal = true
+
+    expect(response.site_admin).to.equal(expVal)
+  })
+
+  // Check api status code
+  it("Api should return a status code of 200", function done () {
+    const response = api.getApi();
+    chai
+      .request(api)
+      .get(response)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        res.status.should.be.equal(200);
+        return done();
+      })
+  })
 })
